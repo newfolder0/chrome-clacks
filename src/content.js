691fd7e88a0da3f8186cf2dc486b7021178da166
newfolder0 <peter@bells.id.au>
2015-03-23 17:20:00 +0100

    minor html meta tag bug fix and improvements

--- src/content.js
+++ src/content.js
@@ -1,15 +1,23 @@
 // Find meta tags with clacks overhead
 // ********** it would be so much better to match this with a regex or similar
 // <meta http-equiv="X-Clacks-Overhead" content="GNU Terry Pratchett" />
-var clacks = document.querySelectorAll("meta[http-equiv='X-Clacks-Overhead']"
-                                    || "meta[http-equiv='x-clacks-overhead']"
-                                    || "meta[http-equiv='Clacks-Overhead']"
-                                    || "meta[http-equiv='clacks-overhead']"),
-    str = "";
+var clacks = document.querySelectorAll("meta"),
+    str = "",
+    // match case-insensitive, with or without 'X-' prefix
+    pattern = /^(X-)?(Clacks-Overhead)$/i;
 
-// get text content from clack meta tags, first item outside loop to avoid newline
-if (clacks[0]) str += clacks[0].getAttribute('content');
-for (i=1; i<clacks.length; i++) str += "\n"+clacks[i].getAttribute('content');
+// function nodeListToArray(nodeList) {
+//     return [].slice.call(nodeList, 0);
+// }
+
+str = [].slice.call(clacks, 0)
+    .filter(function(meta) {
+        return pattern.test(meta.getAttribute("http-equiv"));
+    })
+    .map(function(node) {
+		return node.getAttribute("content");
+	})
+    .join("\n");
 
 // send clacks to background, even if empty, to trigger pageAction check
 chrome.runtime.sendMessage({clacks: str});
