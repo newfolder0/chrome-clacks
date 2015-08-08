a944fe068945b9902a23865e0d4c0d370d8264e2
Peter Bell <peter@bells.id.au>
2015-08-08 17:53:14 +1000

    Added animated icon to popup

--- src/popup.js
+++ src/popup.js
@@ -5,6 +5,7 @@ var tabId,
 chrome.tabs.query(
     {active: true, lastFocusedWindow: true},
     function(tabs) {
+        console.log("tabs: ", tabs);
         // get the tab ID of that current tab
         tabId = tabs[0].id;
         // get clacks headers from background script
@@ -13,6 +14,31 @@ chrome.tabs.query(
                 clacks = bg.getClacks(tabId);
                 // put text in popup.html
                 document.getElementById("text").textContent = clacks;
+
+                // put icon in popup.html
+                // no idea why this doesn't work
+                var iconURL = chrome.extension.getURL("images/backspindle_icons/Clacks19/Clacks-Blank-small.png");
+                console.log(iconURL);
+                document.getElementById("icon").src = iconURL;
+
+                // animate
+                var letterIndex = 0;
+
+                var icon = setInterval(function() {
+                    console.log(clacks[letterIndex]);
+
+                    var str;
+
+                    if (letterIndex == clacks.length) str = "Blank";
+                    else if (clacks[letterIndex] == ' ') str = "SPACE";
+                    else str = clacks[letterIndex].toUpperCase();
+
+                    iconURL = "images/backspindle_icons/Clacks19/Clacks-" + str + "-small.png";
+                    document.getElementById("icon").src = iconURL;
+
+                    letterIndex++;
+                    if (letterIndex > clacks.length) letterIndex = 0;
+                }, 500);
             }
         );
     }
