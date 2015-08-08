abac87a7aff06ea20a0bf6b5821641cc44e0150b
Peter Bell <peter@bells.id.au>
2015-08-08 18:05:36 +1000

    tidied up after adding animation, formatted html

--- src/popup.js
+++ src/popup.js
@@ -15,30 +15,33 @@ chrome.tabs.query(
                 // put text in popup.html
                 document.getElementById("text").textContent = clacks;
 
+                // animate icon
+                var letterIndex = 0;
+                var interval = 500;
+                // var iconSize = 19; // icon size, 19 or 38 pixels
+
                 // put icon in popup.html
-                // no idea why this doesn't work
                 var iconURL = chrome.extension.getURL("images/backspindle_icons/Clacks19/Clacks-Blank-small.png");
                 console.log(iconURL);
                 document.getElementById("icon").src = iconURL;
 
-                // animate
-                var letterIndex = 0;
-
+                // animator loop
                 var icon = setInterval(function() {
-                    console.log(clacks[letterIndex]);
-
                     var str;
 
+                    // get icon name
                     if (letterIndex == clacks.length) str = "Blank";
                     else if (clacks[letterIndex] == ' ') str = "SPACE";
                     else str = clacks[letterIndex].toUpperCase();
 
-                    iconURL = "images/backspindle_icons/Clacks19/Clacks-" + str + "-small.png";
+                    // make full string and set src in html
+                    iconURL = chrome.extension.getURL("images/backspindle_icons/Clacks19/Clacks-" + str + "-small.png");
                     document.getElementById("icon").src = iconURL;
 
+                    // iterate
                     letterIndex++;
                     if (letterIndex > clacks.length) letterIndex = 0;
-                }, 500);
+                }, interval);
             }
         );
     }
